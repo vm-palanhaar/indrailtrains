395669c4b02b66320a3802d7a3eb9822cc1ac97f
Soumay Gupta <118595898+soumayg@users.noreply.github.com>
2023-11-27 11:59:25 +0530

    trains api intg

--- trains.go
+++ trains.go
@@ -51,8 +51,35 @@ func getRailTrainsApi() []Train {
 	return railTrainList
 }
 
+func getRailTrainsDb(db *sql.DB) []Train {
+	table := os.Getenv("TABLE")
+	// execute query
+	log.Println("EXECUTING QUERY to train no, train name")
+	rows, err := db.Query(fmt.Sprintf("SELECT train_no, train_name FROM %s", table))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	trainsDb := []Train{}
+	for rows.Next() {
+		rowData := Train{}
+		err := rows.Scan(&rowData.No, &rowData.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		trainsDb = append(trainsDb, rowData)
+	}
+	log.Printf("Total no. of rail trains (DB): %d", len(trainsDb))
+	return trainsDb
+}
+
 func railTrains(db *sql.DB) {
 	log.Println("START -> GET ALL rail trains API functionality <- START")
 	getRailTrainsApi()
 	log.Println("END -> GET ALL rail trains API functionality <- END")
+
+	log.Println("START -> GET ALL rail trains DB functionality <- START")
+	getRailTrainsDb(db)
+	log.Println("END -> GET ALL rail trains DB functionality <- END")
 }
